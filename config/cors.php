a66e66447c36e1e8e3ec40d41febae08f81c14f6
rc4317809 <rc4317809@gmail.com>
2024-10-22 06:34:40 +0800

    allowed all methods in cors for debugging purposes

--- config/cors.php
+++ config/cors.php
@@ -6,14 +6,16 @@ return [
 
     'allowed_methods' => ['*'], // Allow all methods
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'], // Allow all origins temporarily for debugging purposes
 
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Accept', 'Authorization'],
+    'allowed_headers' => ['*'], // Allow all headers temporarily for debugging purposes
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // Set this to true if you need credentials
+    'supports_credentials' => true, // Set to true if needed, or false otherwise
 
 ];
+
+
